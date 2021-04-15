464c84dd22476e2c24e29eba73bdf6e912e4ec41
Pablo <plaptewww@gmail.com>
2021-04-15 17:04:14 +0300

    added filter fun and classes

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -25,13 +25,17 @@ export default class App extends Component {
                 {label: 'Челика ебнуло градом, пиздец', important: false, like:false, id: 1},
                 {label:'Выпил водки ебать уууу', important:false, like:false, id: 2},
                 {label: 'Ну сука, ну банедри', important:true, like:false, id: 3}
-            ]
+            ], 
+            searchTerm: '',
+            filter:'all' 
         }
 
         this.deleteItem = this.deleteItem.bind(this);
         this.addItem = this.addItem.bind(this);
         this.onToggleImportant = this.onToggleImportant.bind(this);
         this.onToggleLike = this.onToggleLike.bind(this);
+        this.onUpdateSearch = this.onUpdateSearch.bind(this);
+        this.onFilterSelect = this.onFilterSelect.bind(this);
         this.maxId = 4; //отсюда идет генерация новых айди
 
     }
@@ -121,11 +125,40 @@ export default class App extends Component {
         })
     }
 
+    searchPost(items, searchTerm){
+        if (searchTerm.length === 0) {
+            return items;
+        }
+
+       return items.filter( (item)=> {
+            return item.label.indexOf(searchTerm) > -1 //возвращаем те посты в которых строка юзера
+        });
+    }
+
+    filterPost(items, filter){
+        if(filter === 'like'){
+            return items.filter(item=>item.like)
+        }
+        else {
+            return items
+        }
+    }
+
+    onUpdateSearch(searchTerm){
+        this.setState({searchTerm});
+    }
+
+    onFilterSelect(filter){
+        this.setState({filter});
+    }
+
     render() {
 
-        const {data} = this.state;
+        const {data, searchTerm, filter} = this.state;
         const liked = data.filter(item => item.like).length //если лайк тру, то возвращаем, прохд по каждому элементу
         const allPosts = data.length //количество постов всего
+
+        const visiblePosts = this.filterPost(this.searchPost(data, searchTerm), filter);
 
         return (
             <AppBlock>
@@ -133,11 +166,13 @@ export default class App extends Component {
                  liked={liked}
                  allPosts={allPosts}/>
                  <div className ="search-panel d-flex">
-                     <SearchPanel/>
-                     <PostStatusFilter/>
+                     <SearchPanel onUpdateSearch={this.onUpdateSearch}/> {/* Обновление стейта */}
+                     <PostStatusFilter
+                     filter={filter}
+                     onFilterSelect={this.onFilterSelect}/>
                  </div>
                  <PostList 
-                 posts={this.state.data}
+                 posts={visiblePosts}//видимые посты
                  onDelete={this.deleteItem}
                  onToggleImportant={this.onToggleImportant}
                  onToggleLike={this.onToggleLike} />
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './search-panel.css';
 
 
-const SearchPanel = () => {
-    return (
-        <input
-            className="form-control search-input search"
-            type="text"
-            placeholder="Поиск по записям"
-            />
-    )
-}
+export default class SearchPanel extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchTerm: ''
+        }
+        this.onUpdateSearch=this.onUpdateSearch.bind(this);
+    }
 
-export default SearchPanel;
+    onUpdateSearch(e){
+        const searchTerm = e.target.value;
+        this.setState({searchTerm}); //стейт не зависит от предыдущего  значения
+        this.props.onUpdateSearch(searchTerm);
+    }
+
+    render(){
+        return (
+            <input
+                className="form-control search-input search"
+                type="text"
+                placeholder="Поиск по записям"
+                onChange={this.onUpdateSearch} //для слежки ввода юзера
+
+                />
+        )
+    }
+    
+}
